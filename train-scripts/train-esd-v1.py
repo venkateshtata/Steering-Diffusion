7313e4f5de5524a928c97d6bda5c20ee4a807a7c
venkateshtata <venkateshtata9@gmail.com>
2024-04-14 10:18:00 +0000

    clean train-esd

--- train-scripts/train-esd-v1.py
+++ train-scripts/train-esd-v1.py
@@ -8,6 +8,7 @@ from einops import rearrange
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
+# from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.util import instantiate_from_config
 import random
 import glob
@@ -16,10 +17,13 @@ import shutil
 import pdb
 import argparse
 from convertModels import savemodelDiffusers
+
 from cldm.model import create_model, load_state_dict
 from cldm.ddim_hacked import DDIMSampler
+
 from annotator.util import resize_image, HWC3
 import einops
+
 from pytorch_lightning import seed_everything
 from annotator.canny import CannyDetector
 
@@ -27,15 +31,31 @@ apply_canny = CannyDetector()
 
 
 def load_image_as_array(image_path):
+    """
+    Loads an image file into a numpy array.
+
+    Parameters:
+        image_path (str): Path to the image file.
+
+    Returns:
+        numpy.ndarray: Image data as a numpy array.
+    """
+    # Open the image file
     with Image.open(image_path) as img:
+        # Convert the image to RGB if it's not already
         img = img.convert('RGB')
+        # Convert the image to a numpy array
         image_array = np.array(img)
+        
         return image_array
 
 
 # Util Functions
-
 def load_model_from_config(config, ckpt, device="cpu", verbose=False):
+    """Loads a model from config and a ckpt
+    if config is a path will use omegaconf to load
+    """
+    
     batch_size = 4
     logger_freq = 300
     learning_rate = 1e-5
@@ -46,24 +66,28 @@ def load_model_from_config(config, ckpt, device="cpu", verbose=False):
     model.learning_rate = learning_rate
     model.sd_locked = sd_locked
     model.only_mid_control = only_mid_control
+
     model.eval()
     model.cond_stage_model.device = device
     return model
 
 
 @torch.no_grad()
-def sample_model(model, sampler, h, w, ddim_steps, scale, ddim_eta, start_code=None, n_samples=1,t_start=-1,log_every_t=None,till_T=None,verbose=True):
+def sample_model(model, sampler, c, h, w, ddim_steps, scale, ddim_eta, start_code=None, n_samples=1,t_start=-1,log_every_t=None,till_T=None,verbose=True):
+    """Sample the model"""
+    
     a_prompt = "best quality, extremely detailed"
     n_prompt = "longbody, lowres,bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality"
-    prompt = prompt
+    prompt = "bird"
     guess_mode = False
     low_threshold = 100
     high_threshold = 200
     n_samples = 1
-
+    
     # For the conditional image init
-    cond_img = load_image_as_array(erase_condition_image)
+    cond_img = load_image_as_array("./test_images/images_bird_canny.png")
     cond_img = resize_image(HWC3(cond_img), 512)
+    
     h, w = 512, 512
     
     # cond_detected_map = np.zeros_like(cond_img, dtype=np.uint8)
@@ -77,26 +101,42 @@ def sample_model(model, sampler, h, w, ddim_steps, scale, ddim_eta, start_code=N
 
     seed = random.randint(0, 65535)
     seed_everything(0)
-
+    
     cond = {"c_concat": [cond_control], "c_crossattn": [model.get_learned_conditioning([prompt + ', ' + a_prompt] * n_samples)]}
     un_cond = {"c_concat": None if guess_mode else [cond_control], "c_crossattn": [model.get_learned_conditioning([n_prompt] * n_samples)]}
-
+    
     shape = (4, h//8, w//8)
-
+        
     model.control_scales = [1 * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else ([1.0] * 13)
     
     samples_ddim, inters = sampler.sample(ddim_steps, n_samples, shape, cond, verbose=False, eta=0.0, unconditional_guidance_scale=9.0, unconditional_conditioning=un_cond)
+    # print("Got Samples!!")
+    # print("shape of samples: ", samples_ddim.shape)
 
     if log_every_t is not None:
         return samples_ddim, inters
+    
     return samples_ddim
+
+
+def load_img(path, target_size=512):
+    """Load an image, resize and output -1..1"""
+    image = Image.open(path).convert("RGB")
+
+
+    tform = transforms.Compose([
+            transforms.Resize(target_size),
+            transforms.CenterCrop(target_size),
+            transforms.ToTensor(),
+        ])
+    image = tform(image)
+    return 2.*image - 1.
 
 
 def moving_average(a, n=3) :
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
-
 
 def plot_loss(losses, path,word, n=100):
     v = moving_average(losses, n)
@@ -107,7 +147,7 @@ def plot_loss(losses, path,word, n=100):
     plt.ylabel('Loss value', fontsize=16)
     plt.savefig(path)
 
-
+##################### ESD Functions
 def get_models(config_path, ckpt_path, devices):
     model_orig = load_model_from_config(config_path, ckpt_path, devices[1])
     sampler_orig = DDIMSampler(model_orig)
@@ -117,11 +157,53 @@ def get_models(config_path, ckpt_path, devices):
 
     return model_orig, sampler_orig, model, sampler
 
-
 def train_esd(prompt, train_method, start_guidance, negative_guidance, iterations, lr, config_path, ckpt_path, diffusers_config_path, devices, seperator=None, image_size=512, ddim_steps=50):
+    '''
+    Function to train diffusion models to erase concepts from model weights
 
+    Parameters
+    ----------
+    prompt : str
+        The concept to erase from diffusion model (Eg: "Van Gogh").
+    train_method : str
+        The parameters to train for erasure (ESD-x, ESD-u, full, selfattn).
+    start_guidance : float
+        Guidance to generate images for training.
+    negative_guidance : float
+        Guidance to erase the concepts from diffusion model.
+    iterations : int
+        Number of iterations to train.
+    lr : float
+        learning rate for fine tuning.
+    config_path : str
+        config path for compvis diffusion format.
+    ckpt_path : str
+        checkpoint path for pre-trained compvis diffusion weights.
+    diffusers_config_path : str
+        Config path for diffusers unet in json format.
+    devices : str
+        2 devices used to load the models (Eg: '0,1' will load in cuda:0 and cuda:1).
+    seperator : str, optional
+        If the prompt has commas can use this to seperate the prompt for individual simulataneous erasures. The default is None.
+    image_size : int, optional
+        Image size for generated images. The default is 512.
+    ddim_steps : int, optional
+        Number of diffusion time steps. The default is 50.
+
+    Returns
+    -------
+    None
+
+    '''
     # PROMPT CLEANING
     word_print = prompt.replace(' ','')
+    if prompt == 'allartist':
+        prompt = "Kelly Mckernan, Thomas Kinkade, Ajin Demi Human, Alena Aenami, Tyler Edlin, Kilian Eng"
+    if prompt == 'i2p':
+        prompt = "hate, harassment, violence, suffering, humiliation, harm, suicide, sexual, nudity, bodily fluids, blood"
+    if prompt == "artifact":
+        prompt = "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, blurred, text, watermark, grainy"
+
     if seperator is not None:
         words = prompt.split(seperator)
         words = [word.strip() for word in words]
@@ -129,6 +211,7 @@ def train_esd(prompt, train_method, start_guidance, negative_guidance, iteration
         words = [prompt]
     print(words)
     ddim_eta = 0
+    # MODEL TRAINING SETUP
 
     model_orig, sampler_orig, model, sampler = get_models(config_path, ckpt_path, devices)
 
@@ -175,8 +258,8 @@ def train_esd(prompt, train_method, start_guidance, negative_guidance, iteration
     model.train()
     
     # create a lambda function for cleaner use of sampling code (only denoising till time step t)
-    quick_sample_till_t = lambda s, code, t: sample_model(model, sampler,
-                                                                 image_size, image_size, ddim_steps, s, ddim_eta,
+    quick_sample_till_t = lambda x, s, code, t: sample_model(model, sampler,
+                                                                 x, image_size, image_size, ddim_steps, s, ddim_eta,
                                                                  start_code=code, till_T=t, verbose=False)
 
     losses = []
@@ -184,11 +267,17 @@ def train_esd(prompt, train_method, start_guidance, negative_guidance, iteration
     criteria = torch.nn.MSELoss()
     history = []
 
-    name = f'controlnet-word_{word_print}-method_{train_method}-sg_{start_guidance}-ng_{negative_guidance}-iter_{iterations}-lr_{lr}'
+    name = f'compvis-word_{word_print}-method_{train_method}-sg_{start_guidance}-ng_{negative_guidance}-iter_{iterations}-lr_{lr}'
     # TRAINING CODE
     pbar = tqdm(range(iterations))
     for i in pbar:
         word = random.sample(words,1)[0]
+        # get text embeddings for unconditional and conditional prompts
+        emb_0 = model.get_learned_conditioning([''])
+        emb_p = model.get_learned_conditioning([word])
+        emb_n = model.get_learned_conditioning([f'{word}'])
+        
+        print("emb_0: ", emb_0.shape)
 
         opt.zero_grad()
 
@@ -203,7 +292,7 @@ def train_esd(prompt, train_method, start_guidance, negative_guidance, iteration
 
         with torch.no_grad():
             # generate an image with the concept from ESD model
-            z = quick_sample_till_t(start_guidance, start_code, int(t_enc)) # emb_p seems to work better instead of emb_0
+            z = quick_sample_till_t(emb_p.to(devices[0]), start_guidance, start_code, int(t_enc)) # emb_p seems to work better instead of emb_0
             # print("shape of z: ", z.shape)
             
             #Get outputs from frozen model
@@ -236,8 +325,8 @@ def train_esd(prompt, train_method, start_guidance, negative_guidance, iteration
             # print("shape of e_0: ", e_0.shape)
             
             # get Conditional scores from frozen model at time step t and image z
-            cprompt = prompt
-            cond_img = load_image_as_array(erase_condition_image)
+            cprompt = "bird"
+            cond_img = load_image_as_array("./test_images/images_bird_canny.png")
             cond_img = resize_image(HWC3(cond_img), 512)
             
             h, w = 512, 512
@@ -258,8 +347,8 @@ def train_esd(prompt, train_method, start_guidance, negative_guidance, iteration
             
         # breakpoint()
         # get conditional score from ESD model
-        e_prompt = prompt
-        e_cond_img = load_image_as_array(erase_condition_image)
+        e_prompt = "bird"
+        e_cond_img = load_image_as_array("./test_images/images_bird_canny.png")
         e_cond_img = resize_image(HWC3(e_cond_img), 512)
         
         h, w = 512, 512
@@ -335,8 +424,8 @@ if __name__ == '__main__':
     parser.add_argument('--negative_guidance', help='guidance of negative training used to train', type=float, required=False, default=1)
     parser.add_argument('--iterations', help='iterations used to train', type=int, required=False, default=600)
     parser.add_argument('--lr', help='learning rate used to train', type=int, required=False, default=1e-5)
-    parser.add_argument('--config_path', help='config path for stable diffusion v1-4 inference', type=str, required=False, default='./configs/controlnet/cldm_v15.yaml')
-    parser.add_argument('--ckpt_path', help='ckpt path for stable diffusion v1-4', type=str, required=False, default='./models/ldm/controlnet_canny/control_sd15_canny.pth')
+    parser.add_argument('--config_path', help='config path for stable diffusion v1-4 inference', type=str, required=False, default='configs/controlnet/cldm_v15.yaml')
+    parser.add_argument('--ckpt_path', help='ckpt path for stable diffusion v1-4', type=str, required=False, default='models/ldm/controlnet_canny/control_sd15_canny.pth')
     # parser.add_argument('--config_path', help='config path for stable diffusion v1-4 inference', type=str, required=False, default='configs/stable-diffusion/v1-inference.yaml')
     # parser.add_argument('--ckpt_path', help='ckpt path for stable diffusion v1-4', type=str, required=False, default='models/ldm/stable-diffusion-v1/sd-v1-4-full-ema.ckpt')
     parser.add_argument('--diffusers_config_path', help='diffusers unet config json path', type=str, required=False, default='diffusers_unet_config.json')
@@ -344,8 +433,6 @@ if __name__ == '__main__':
     parser.add_argument('--seperator', help='separator if you want to train bunch of words separately', type=str, required=False, default=None)
     parser.add_argument('--image_size', help='image size used to train', type=int, required=False, default=512)
     parser.add_argument('--ddim_steps', help='ddim steps of inference used to train', type=int, required=False, default=50)
-    
-    parser.add_argument('--erase_condition_image', help='Erase Condition Image', type=str, required=True, default="")
     args = parser.parse_args()
     
     prompt = args.prompt
@@ -361,7 +448,5 @@ if __name__ == '__main__':
     seperator = args.seperator
     image_size = args.image_size
     ddim_steps = args.ddim_steps
-    
-    erase_condition_image = args.erase_condition_image
 
     train_esd(prompt=prompt, train_method=train_method, start_guidance=start_guidance, negative_guidance=negative_guidance, iterations=iterations, lr=lr, config_path=config_path, ckpt_path=ckpt_path, diffusers_config_path=diffusers_config_path, devices=devices, seperator=seperator, image_size=image_size, ddim_steps=ddim_steps)
