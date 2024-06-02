38c9f42417e2752c46e9775a59c8bba6218fce83
venkateshtata <venkateshtata9@gmail.com>
2024-06-02 21:51:15 +0000

    updated preprocessing

--- test_scripts/test_model.py
+++ test_scripts/test_model.py
@@ -1,11 +1,16 @@
-from PIL import Image
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
-import torch
-from controlnet_aux import HEDdetector
-from diffusers.utils import load_image
-import einops
+from share import *
 
-from annotator.util import resize_image, HWC3
+from cldm.model import create_model, load_state_dict
+import cv2
+from annotator.util import resize_image
+import numpy as np
+import torch
+import einops
+from cldm.ddim_hacked import DDIMSampler
+from PIL import Image
+from annotator.util import HWC3
+import random
+from pytorch_lightning import seed_everything
 from annotator.hed import HEDdetector, nms
 import numpy as np
 import cv2
@@ -17,6 +22,40 @@ def load_image_as_array(image_path):
         img = img.convert('RGB')
         image_array = np.array(img)
         return image_array
+
+
+object_class = "dog"
+input_type = "canny"
+model_used = "scribble"
+unconditional_guidance_scale = 7
+res = 512
+
+# Configs
+# resume_path = '/notebooks/erase/stable-diffusion/models/ldm/controlnet_scribble/control_sd15_scribble.pth' # your checkpoint path
+resume_path = "/notebooks/Steering-Diffusion/models/test-compvis-word_airplane-method_full-sg_3-ng_1-iter_500-lr_1e-05_scribble/test-compvis-word_airplane-method_full-sg_3-ng_1-iter_500-lr_1e-05_scribble.pt"
+N = 1
+ddim_steps = 50
+
+
+model = create_model('/notebooks/Steering-Diffusion/configs/controlnet/cldm_v15.yaml').cpu()
+model.load_state_dict(load_state_dict(resume_path, location='cuda'), strict=False)
+model = model.cuda()
+sampler = DDIMSampler(model)
+output_img_path = f'{object_class}-prompt_{object_class}-{input_type}_{unconditional_guidance_scale}-gs_{model_used}-notime.png'
+# output_img_path = 'fish_test.png'
+
+a_prompt = "best quality, extremely detailed"
+n_prompt = "worst quality, low quality, noisy"
+prompt = object_class
+guess_mode = False
+n_samples=1
+h = res
+w = res
+
+low_threshold = 100
+high_threshold = 200
+
+# For the conditional image init
 
 image = load_image_as_array("/notebooks/Steering-Diffusion/test_input_images/airplane_sketch.png")
 
@@ -35,26 +74,28 @@ detected_map[detected_map < 255] = 0
 
 control = torch.from_numpy(detected_map.copy()).float().cuda() / 255.0
 control = torch.stack([control for _ in range(1)], dim=0)
-control = einops.rearrange(control, 'b h w c -> b c h w').clone()
+cond_control = einops.rearrange(control, 'b h w c -> b c h w').clone()
+
+seed = random.randint(0, 65535)
+seed_everything(0)
+
+cond = {"c_concat": [cond_control], "c_crossattn": [model.get_learned_conditioning([prompt + ', ' + a_prompt] * n_samples)]}
+un_cond = {"c_concat": None if guess_mode else [cond_control], "c_crossattn": [model.get_learned_conditioning([n_prompt] * n_samples)]}
+
+shape = (4, h//8, w//8)
+
+model.control_scales = [1 * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else ([1.0] * 13)
+
+samples, inters = sampler.sample(ddim_steps, n_samples, shape, cond, verbose=False, eta=0.0, unconditional_guidance_scale=unconditional_guidance_scale, unconditional_conditioning=None)
 
 
+print("samples: ", samples.shape)
+x_samples = model.decode_first_stage(samples)
+x_samples = x_samples.squeeze(0)
+x_samples = (x_samples + 1.0) / 2.0
+x_samples = x_samples.transpose(0, 1).transpose(1, 2)
+x_samples = x_samples.cpu().numpy()
+x_samples = (x_samples * 255).astype(np.uint8)
 
-
-controlnet = ControlNetModel.from_pretrained(
-    "/notebooks/Steering-Diffusion/models/diff_models/compvis-word_airplane-method_notime-sg_3-ng_1-iter_500-lr_1e-05_scribble", torch_dtype=torch.float16
-)
-
-pipe = StableDiffusionControlNetPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
-)
-
-pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-
-# Remove if you do not have xformers installed
-# pipe.enable_xformers_memory_efficient_attention()
-
-pipe.enable_model_cpu_offload()
-
-image = pipe("airplane", control, num_inference_steps=50).images[0]
-
-image.save('airplane_scribble_out.png')
+image_name = output_img_path.split('/')[-1]
+Image.fromarray(x_samples).save(image_name)
